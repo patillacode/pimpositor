ff730724fc14a3ab8f473c87d0f445b8ccb88c4e
Michael Klich <michal@michalklich.com>
2015-12-05 21:36:28 +0000

    Add setup, manifest

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pimpositor',
+    version='0.0.4',
+    description='',
+    license='GPLv2',
+    include_package_data=True,
+    packages=find_packages(),
+    author='patillacode',
+    author_email='your@email.com',
+    url='https://github.com/patillacode/pimpositor',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v2',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7']
+)
