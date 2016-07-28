85be396d80b6f2856e67b1ecd54b81a711b23af0
Gonzalo Fernandez <patillacode@gmail.com>
2016-07-28 10:52:14 +0100

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pimpositor',
-    version='0.0.4',
+    version='0.0.5',
     description='',
     license='GPLv2',
     include_package_data=True,
